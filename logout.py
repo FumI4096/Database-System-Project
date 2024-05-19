1f5bde652188fc66ff3258e401c2e60aab1cca7f
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-05-19 11:40:40 +0800

    Added files and their functions

--- logout.py
+++ logout.py
@@ -7,7 +7,7 @@ ui.set_default_color_theme("blue")
 
 app = ui.CTk()
 app.geometry("600x400")
-app.title("Register")
+app.title("Log Out")
 
 ####################
 def login():
@@ -22,7 +22,7 @@ def login():
         
         query = (f"SELECT ID, FirstName, LastName FROM Registration WHERE ID = {int(enterId.get())}")
         cursor.execute(query)
-        proc = ("EXEC spUpdateLogIN @ID = ?")
+        proc = ("EXEC spUpdateLogOUT @ID = ?")
         
         result = cursor.fetchone()
         
@@ -36,8 +36,7 @@ def login():
         S = (set.strftime("%S"))
         P = (set.strftime("%p"))
         time = f"{H}:{M}:{S} {P}"
-
-        hello
+        
         if result:
             id, first_name, last_name = result
             print("Result from stored procedure:", id)
@@ -45,35 +44,21 @@ def login():
             attendanceOutput.configure(text=f"{first_name} {last_name}\n({date} {time})")
             
         else:
-            print("No result returned from stored procedure", result[0])
-
-        # if result:
-        #     cond = result[0]
-        #     print(cond)
-        #     if cond == 1:
-        #         attendanceOutput.configure(text="Attendance Done")
-        #     elif cond == 0:
-        #         attendanceOutput.configure(text="ID not found in LoggingIn")
-        #     elif cond == -1:
-        #         attendanceOutput.configure(text="ID not found in Registration")
-        #     else:
-        #         attendanceOutput.configure(text="Unexpected condition value")
-        # else:
-        #     attendanceOutput.configure(text="No result returned")
+            attendanceOutput.configure(text="Enter a valid ID")
         
         attendanceOutput.after(6000, lambda: attendanceOutput.configure(text=""))
     except pdb.Error as ex:
         print('Connection failed', ex)
         attendanceOutput.configure(text="Error Occured")
-        attendanceOutput.after(3000, lambda: attendanceOutput.configure(text=""))
+        attendanceOutput.after(6000, lambda: attendanceOutput.configure(text=""))
 
 attendanceOutput = ui.CTkLabel(app, text="")
-attendanceOutput.place(relx = 0.34, rely = 0.6)
+attendanceOutput.place(relx = 0.37, rely = 0.2)
 
 enterId = ui.CTkEntry(app, placeholder_text="Enter ID", width=200, height=50, border_width=1, border_color="black")
-enterId.place(relx = 0.34, rely = 0.3)
+enterId.place(relx = 0.34, rely = 0.5)
 
 submit = ui.CTkButton(app, text="SUBMIT", fg_color="blue", width=100, height=40, command=login)
-submit.place(relx = 0.34, rely = 0.5)
+submit.place(relx = 0.34, rely = 0.7)
 
 app.mainloop()
